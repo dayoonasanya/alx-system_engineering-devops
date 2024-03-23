bce98ef3f4b6f967375916189724519bd1e7a177
dayoonasanya <aonasanya@mail.dccc.edu>
2024-03-22 17:03:34 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# Installs puppet-lint, version 2.1.1
+# Install an especific version of flask (2.1.0)
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
